61a638a011f2d003bd41095a7a69630ad0368c07
ww <wangweicomcn163@gmail.com>
2017-01-24 14:57:14 -0500

    finish rf model

--- digit_recognizer.r
+++ digit_recognizer.r
@@ -1,9 +1,14 @@
 #this script is the classifer for digit recognization. 
 
+#configure multicore
+library
 
 #load training and testing data sets
 training <- read.csv('Digit recognizer/train.csv')
 testing <- read.csv('Digit recognizer/test.csv')
+
+#convert labels to factors
+training$label <- as.factor(training$label)
 
 #display the first thirty samples from training dataset
 par(mar = c(rep(0.1, 4)))   #set margin to 0.1
@@ -18,5 +23,25 @@ for (i in 1:30) {
 
 #train rf model
 library(caret)
-ctrl <- trainControl(method='repeatedcv',number=10,repeats=1)
-modalfit <- train(label~.,data=training,method='rf',trControl=ctrl)
+ctrl <- trainControl(method = 'repeatedcv',
+                     number = 10,
+                     repeats = 1)
+modelfit <-
+        train(label ~ .,
+              data = training[sample(1:42000, 1000), ],
+              method = 'rf',
+              trControl = ctrl)
+
+#predict
+pred <- predict(modelfit,testing)
+
+#display the first thirty samples from testing dataset
+par(mar = c(rep(0.1, 4)))   #set margin to 0.1
+par(mfrow = c(6, 5))   #set plot array to 6 (row) by 5 (col)
+for (i in 1:30) {
+        spl <- testing[i,]
+        spl <- unlist(spl)
+        dim(spl) <- c(28, 28)
+        image(spl[, nrow(spl):1], axes = FALSE,
+              col = grey(seq(0, 1, length = 256)))
+}
