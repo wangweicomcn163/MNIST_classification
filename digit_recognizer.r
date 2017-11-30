104ebc96ebc5f02f3a9b72648b5c0c4fee7cea4c
wangweicomcn163 <wangweicomcn163@gmail.com>
2017-11-30 14:20:14 -0500

    update digit_recognizer.r

--- digit_recognizer.r
+++ digit_recognizer.r
@@ -5,28 +5,31 @@ rawdata <- read.csv('Digit recognizer/train.csv')
 #testing <- read.csv('Digit recognizer/test.csv')
 
 
+
+#=================split raw data===============================================
+rawdata$label <- as.factor(rawdata$label)
+set.seed(2017-03-21)
+intrain <- createDataPartition(rawdata$label,p=0.8,list = FALSE)
+training <- rawdata[intrain,]
+testing <- rawdata[-intrain,]
+
+
+
+#=================function to display image====================================
+display <- function(x){
+        x <- unlist(x[,2:785])
+        dim(x) <- c(28, 28)
+        image(x[, nrow(x):1], axes = FALSE,
+              col = grey(seq(0, 1, length = 256)))
+}
+
 #=================Display sample images========================================
 #display the first thirty samples from training dataset
 par(mar = c(rep(0.1, 4)))   #set margin to 0.1
 par(mfrow = c(6, 5))   #set plot array to 6 (row) by 5 (col)
 for (i in 1:30) {
-        spl <- rawdata[i, 2:785]
-        spl <- unlist(spl)
-        dim(spl) <- c(28, 28)
-        image(spl[, nrow(spl):1], axes = FALSE,
-              col = grey(seq(0, 1, length = 256)))
+        display(training[i,])
 }
-
-#=================preprocess data==============================================
-rawdata$label <- as.factor(rawdata$label)
-zerovaridx <- nearZeroVar(rawdata,saveMetrics = TRUE)
-rawdata_proc <- rawdata[,which(!zerovaridx$zeroVar)] #remove zerovar variables
-
-#=================split raw data===============================================
-intrain <- createDataPartition(rawdata_proc$label,p=0.75,list = FALSE)
-training <- rawdata_proc[intrain,]
-testing <- rawdata_proc[-intrain,]
-
 
 
 #=================train rf in caret============================================
@@ -37,7 +40,7 @@ ctrl <- trainControl(method = 'repeatedcv',
                      number = 2,
                      repeats = 1,
                      verboseIter=TRUE,
-                     allowParallel = TRUE)
+                     allowParallel = F)
 
 #use multicore and caret packages for parallel computing
 ptm <- proc.time()  #start the clock
@@ -64,11 +67,14 @@ confusionMatrix(pred.rf,testing$label)
 cl <- makeCluster(2) #register 2 cores
 registerDoSNOW(cl)
 ptm <- proc.time()  #start the clock
+set.seed(234)
 rf <- foreach(ntree = rep(50, 3),
                 .combine = combine,
-                .packages = 'randomForest') %dopar% {
+                .packages = 'randomForest',
+                .verbose=TRUE) %dopar% {
                         randomForest(training[,-1], training[, 1], 
-                                     ntree = ntree)
+                                     ntree = ntree,
+                                     do.trace = TRUE)
                 }
 stopCluster(cl)
 proc.time()-ptm   #stop the clock
@@ -76,20 +82,3 @@ proc.time()-ptm   #stop the clock
 confusionMatrix(predict(rf,testing),testing$label)
 
 #training time~570sec,accuracy 96.4
-
-
-
-
-#=================Display sample images========================================
-#display the first thirty samples from testing dataset
-par(mar = c(rep(0.1, 4)))   #set margin to 0.1
-par(mfrow = c(6, 5))   #set plot array to 6 (row) by 5 (col)
-for (i in 1:30) {
-        spl <- testing[i,]
-        spl <- unlist(spl)
-        dim(spl) <- c(28, 28)
-        image(spl[, nrow(spl):1], axes = FALSE,
-              col = grey(seq(0, 1, length = 256)))
-}
-
-dev.off() #reset the device to default.
